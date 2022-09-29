b3f9f20880505c043d5d129d94e4d5edf08b5bba
ugix-sassiness <78940245+ugix-sassiness@users.noreply.github.com>
2022-09-29 12:39:22 -0500

    integrated calendly widget inline

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
+import React from 'react';
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { InlineWidget } from 'react-calendly';
+
+
+const Scheduler = () => {
+  return (
+    <div className='Scheduler'>
+      <InlineWidget url="https://calendly.com/food-signel/30min"/>
+    </div>
+  );
+};
+
 
 export default function Home() {
   return (
@@ -15,6 +27,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <Scheduler/>
       </main>
 
       <Footer />
